ed6863f0785afbb48236c644b10f61f852209a84
iamrupnath <change.01life@gmail.com>
2024-03-18 12:53:18 +0530

    backend setup

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,23 @@
-const express = require('express')
-
+const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 
-if(process.env.NODE_ENV != 'production'){
-    require("dotenv").config({
-        path:"backend/config/.env",
-    })
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(fileUpload({ useTempFiles: true }));
+
+//app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config({
+    path: "backend/config/.env",
+  });
 }
+
+// it's for ErrorHandling
+// app.use(ErrorHandler);
 
 module.exports = app;
