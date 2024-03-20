815bf7a7eb8f17145c69f09fc0765ae15ca1e57f
iamrupnath <change.01life@gmail.com>
2024-03-20 23:41:46 +0530

    navbar

---
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -0,0 +1,18 @@
+export const mainCarouselData = [
+  {
+    image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_mob_5.jpg",
+    path: "/woman/clothing/lehenga-choli",
+  },
+  {
+    image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_12.jpg",
+    path: "/woman/clothing/lehenga",
+  },
+  {
+    image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_10.jpg",
+    path: "/woman/clothing/lehenga-choli",
+  },
+  {
+    image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_mob_4.jpg",
+    path: "/woman/clothing/lehenga-choli",
+  },
+];
---
+++ src/customer/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MainCarousel from "../../components/HomeCarousel/MainCarousel";
+
+function HomePage() {
+  return (
+    <>
+      <div>
+        <MainCarousel />
+      </div>
+    </>
+  );
+}
+
+export default HomePage;
