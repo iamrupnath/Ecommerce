260a719ffa5852192a01c30fdf73695b9c981f28
iamrupnath <change.01life@gmail.com>
2024-03-21 00:22:19 +0530

    MainCarousel data

--- src/customer/components/HomeCarousel/MainCarouselData.js
+++ src/customer/components/HomeCarousel/MainCarouselData.js
@@ -1,18 +1,18 @@
 export const mainCarouselData = [
   {
-    image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_mob_5.jpg",
+    image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_12.jpg",
     path: "/woman/clothing/lehenga-choli",
   },
   {
     image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/0/10_12.jpg",
-    path: "/woman/clothing/lehenga",
+    path: "/woman/clothing/women_dress",
   },
   {
     image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_10.jpg",
     path: "/woman/clothing/lehenga-choli",
   },
   {
-    image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_mob_4.jpg",
-    path: "/woman/clothing/lehenga-choli",
+    image:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/2/12_10.jpg",
+    path: "/woman/clothing/women-saree",
   },
 ];
