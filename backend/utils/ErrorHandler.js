a284714291309334c3ba53a0cbe26abe5c9ef14e
iamrupnath <change.01life@gmail.com>
2024-03-20 21:12:17 +0530

    first

--- backend/utils/ErrorHandler.js
+++
@@ -1,8 +0,0 @@
-function ErrorHandler(message, statusCode) {
-  const error = new Error(message);
-  error.statusCode = statusCode;
-  Error.captureStackTrace(error, ErrorHandler);
-  return error;
-}
-
-module.exports = ErrorHandler;
--- frontend/src/routes/Routes.js
+++
@@ -1,7 +0,0 @@
-import LoginPage from "../pages/LoginPage";
-import SignupPage from "../pages/SignupPage";
-import ActivationPage from "../pages/ActivationPage";
-
-export { 
-    LoginPage, SignupPage,ActivationPage,
-}
